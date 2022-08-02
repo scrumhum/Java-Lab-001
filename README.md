23965d0f16cb6cc6a0950be5d9846adf7bcd78f0
Trevor Hartman <viable.hartman@gmail.com>
2022-08-01 18:13:53 -0700

    Lab 001 prepared for student cloning. Initial yoda function set up, and default java main method added. Ignore file updated with default IntelliJ ignore patterns. Sharable IntelliJ project files added.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Java-Lab-001
-Java lab 1 for learning hello world!
+Java lab 1 for learning hello yoda!
---
+++ src/HelloYoda.java
@@ -0,0 +1,18 @@
+public class HelloYoda {
+    /**
+     * Returns a String containing a Yoda ASCII art and quote.
+     *
+     * @return Yoda Face and Quote as a String.
+     */
+    static String yoda() {
+        return """
+                          .--.                  Try not.
+                ::\\`--._,'.::.`._.--'/::     Do or do not.
+                ::::.  ` __::__ '  .::::    There is no try.
+                ::::::-:.`'..`'.:-::::::
+                ::::::::\\ `--' /::::::::              -Yoda""";
+    }
+
+    public static void main(String[] args) {
+    }
+}
